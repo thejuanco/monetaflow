6695437a7915f55f4c8f394da6373d72e96b1b8f
Juanco <juanffo874@gmail.com>
2025-09-05 21:02:17 -0600

    Crea el componente del login y lo agrega al router

---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,12 @@
+import NavBarAuth from "../../components/layout/auth/NavBarAuth"
+
+export default function Login() {
+  return (
+    <>
+        <NavBarAuth/>
+        <div>
+            Lgin
+        </div>
+    </>
+  )
+}
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,14 +1,18 @@
 //Este archivo funciona como un router para las rutas de las paginas
 import { BrowserRouter, Routes, Route } from "react-router";
 
-//Paginas
+//Paginas publicas
 import Home from "../pages/Home/Home";
+
+//Paginas privadas
+import Login from "../pages/auth/Login";
 
 export default function Router (){
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/signin" element={<Login/>}/>
             </Routes>
         </BrowserRouter>
     )
