bdb5cde4a30a89b4f3b7da683b81c87688d4428b
Juanco <juanffo874@gmail.com>
2025-09-05 21:02:41 -0600

    Crea el componente del Navbar para la autenticacion

---
+++ src/components/layout/auth/NavBarAuth.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router"
+
+export default function NavBarAuth() {
+  return (
+     <nav className="bg-gray-100 w-full flex justify-between items-center px-6 py-3 fixed top-0 left-0 z-50">
+      <Link to='/' className="font-bold text-2xl">Moneta flow</Link>
+    </nav>
+  )
+}
