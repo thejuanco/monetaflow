dc024ca6c0e8342b062bee91a24b6fa3a8ff1142
Juanco <juanffo874@gmail.com>
2025-09-10 22:44:29 -0600

    Agrega formato

--- src/pages/auth/RecoveryPassword.jsx
+++ src/pages/auth/RecoveryPassword.jsx
@@ -15,27 +15,31 @@ export default function RecoveryPassword() {
             Recupera tu contraseña
           </h1>
           <form onSubmit={() => {}} className="mt-12 mb-4">
-                    <div className="space-y-2 mb-12 ">
-                    <label className="font-semibold" htmlFor="email">
-                        Correo electronico*
-                    </label>
-                    <input
-                        type="email"
-                        name="email"
-                        className={`w-full p-2 rounded-lg border mt-2 focus:outline-none focus:ring-1 ${
-                            errors.email ? "border-red-500 focus:ring-red-500" : "border-gray-200"
-                          }`}
-                        placeholder="Ingresa tu correo electrónico"
-                        // {...register('email', {required: true})}
-                    />
-                    {errors.email?.type === "required" && (
-                        <p role="alert" className="text-red-700 text-sm text-center">El correo es obligatorio</p>
-                    )}
-                    </div>
-                    <button className="bg-blue-700 text-white w-full mt-8 py-2 font-medium rounded-lg hover:bg-blue-600">
-                    Continuar
-                    </button>
-                </form>
+            <div className="space-y-2 mb-12 ">
+              <label className="font-semibold" htmlFor="email">
+                Correo electronico*
+              </label>
+              <input
+                type="email"
+                name="email"
+                className={`w-full p-2 rounded-lg border mt-2 focus:outline-none focus:ring-1 ${
+                  errors.email
+                    ? "border-red-500 focus:ring-red-500"
+                    : "border-gray-200"
+                }`}
+                placeholder="Ingresa tu correo electrónico"
+                // {...register('email', {required: true})}
+              />
+              {errors.email?.type === "required" && (
+                <p role="alert" className="text-red-700 text-sm text-center">
+                  El correo es obligatorio
+                </p>
+              )}
+            </div>
+            <button className="bg-blue-700 text-white w-full mt-8 py-2 font-medium rounded-lg hover:bg-blue-600">
+              Continuar
+            </button>
+          </form>
           <p className="text-sm m-2 mb-4 text-gray-500">
             Te enviaremos un correo a tu cuenta de correo para restablecer tu
             contraseña.
