94f43b31fd868473dcd24910063c8a2ca3b7c2db
Juanco <juanffo874@gmail.com>
2025-09-10 22:44:05 -0600

    Crea el componente Register y lo agrega al router

---
+++ src/pages/auth/Register.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router"
+import NavBarAuth from "../../components/layout/auth/NavBarAuth"
+
+export default function Register() {
+  return (
+    <>
+        <NavBarAuth/>
+    </>
+  )
+}
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -6,6 +6,7 @@ import Home from "../pages/Home/Home";
 import NotFound from "../pages/Home/NotFound";
 import Login from "../pages/auth/Login";
 import RecoveryPassword from "../pages/auth/RecoveryPassword";
+import Register from "../pages/auth/Register";
 
 //Paginas privadas
 
@@ -16,7 +17,7 @@ export default function Router (){
                 <Route index path="/" element={<Home/>}/>
                 <Route path="*" element={<NotFound/>}/>
                 <Route path="/auth/signin" element={<Login/>}/>
-                <Route path="/auth/signup" element={<Login/>}/>
+                <Route path="/auth/signup" element={<Register/>}/>
                 <Route path="/auth/recovery-password" element={<RecoveryPassword/>}/>
             </Routes>
         </BrowserRouter>
