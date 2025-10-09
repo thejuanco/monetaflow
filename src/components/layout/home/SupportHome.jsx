44aa4151047136ed3038ef861167ae52f867788e
Juanco <juanffo874@gmail.com>
2025-10-08 19:29:43 -0600

    Agrega estilos a la página de soporte

--- src/components/layout/home/SupportHome.jsx
+++ src/components/layout/home/SupportHome.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router"
+import NavBarAuth from "../auth/NavBarAuth"
 
 export default function SupportHome() {
   return (
-    <div>SupportHome</div>
+    <>  
+        <NavBarAuth/>
+        <div className="flex justify-center items-center mt-20">
+            <div className="w-full max-w-md border border-gray-100 p-6 rounded-xl shadow">
+                <h1 className="text-black text-center font-bold text-2xl pt-8">
+                    Soporte Técnico
+                </h1>
+            </div>
+        </div>
+    </>
   )
 }
