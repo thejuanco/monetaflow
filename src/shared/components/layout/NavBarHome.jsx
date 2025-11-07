ac74f301e77cb31682b00e5e4caa87fea6e12250
Juanco <juanffo874@gmail.com>
2025-11-06 18:34:22 -0600

    Agrega estilos a los botones para iniciar sesión

--- src/shared/components/layout/NavBarHome.jsx
+++ src/shared/components/layout/NavBarHome.jsx
@@ -14,10 +14,10 @@ export default function NavBarHome() {
         </div>
         <div className="flex flex-1 items-center justify-center space-x-2 md:justify-end">
           <nav className="flex items-center space-x-6 text-sm font-medium">
-            <Link to="/auth/signup" className="hover:text-foreground/80">
+            <Link to="/auth/signup" className="flex items-center font-medium py-2 px-4 rounded-full hover:bg-gray-200 transition-transform duration-300 transform hover:scale-105">
               Crear cuenta
             </Link>
-            <Link to="/auth/signin" className="hover:text-foreground/80">
+            <Link to="/auth/signin" className="flex items-center bg-gray-800 text-white font-medium py-2 px-4 rounded-full hover:bg-gray-900 transition-transform duration-300 transform hover:scale-105">
               Iniciar Sesión
             </Link>
           </nav>
