70bba97ed9fc3bfd8044bcae570a645f79953a9e
Juanco <juanffo874@gmail.com>
2025-11-08 00:58:31 -0600

    Agrega la pantalla de presupuesto

---
+++ src/features/budgets/pages/Budgets.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Budgets() {
+  return (
+    <div className="flex-1 space-y-4 p-4 md:p-4 pt-6">
+        <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-bold tracking-tight">Presupuestos</h2>
+            <div className="flex items-center gap-2">
+                <button>
+                    Nuevo Presupuesto
+                </button>
+            </div>
+        </div>
+
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+            
+        </div>
+    </div>
+  )
+}
+
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -14,6 +14,7 @@ import Support from "../pages/Home/Support";
 import DashboardLayout from "../shared/components/layout/DashboardLayout";
 import Dashboard from "../features/dashboard/pages/Dashboard";
 import Transactions from "../features/transactions/pages/Transactions";
+import Budgets from "../features/budgets/pages/Budgets";
 
 export default function Router (){
     return (
@@ -31,6 +32,7 @@ export default function Router (){
                 <Route element={<DashboardLayout/>}>
                     <Route path="/dashboard" element={<Dashboard/>}/>
                     <Route path="/dashboard/transactions" element={<Transactions/>}/>
+                    <Route path="/dashboard/budgets" element={<Budgets/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
