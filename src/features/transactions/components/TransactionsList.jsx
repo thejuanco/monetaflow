f9402244af73ad053ad023ec7a385c90ab52e33a
Juanco <juanffo874@gmail.com>
2025-11-15 00:19:31 -0600

    Agrega el nuevo componente de transacciones

---
+++ src/features/transactions/components/TransactionsList.jsx
@@ -0,0 +1,8 @@
+
+export default function TransactionsList() {
+  return (
+    <div className="w-full border p-5 border-gray-200">
+        TransactionsList
+    </div>
+  )
+}
--- src/features/transactions/pages/Transactions.jsx
+++ src/features/transactions/pages/Transactions.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import TransactionsList from "../components/TransactionsList"
 
 export default function Transactions() {
   return (
-    <div>Transactions</div>
+    <div className="flex-1 space-y-4 p-4 md:p-4 pt-6">
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-bold tracking-tight">Transacciones</h2>
+        <div className="flex items-center gap-2">
+          <button className="border border-gray-200 px-4 py-2">
+            Nueva Transacción
+          </button>
+        </div>
+      </div>
+
+      {/*Transactions Grid */}
+      <TransactionsList/>
+    </div>
   )
 }
