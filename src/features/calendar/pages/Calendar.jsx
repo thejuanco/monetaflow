2f25bc6c339e7f1af7822a23ce552fcb7f801066
Juanco <juanffo874@gmail.com>
2025-11-17 01:00:55 -0600

    Agrega el encabezado de la vista del calendario

--- src/features/calendar/pages/Calendar.jsx
+++ src/features/calendar/pages/Calendar.jsx
@@ -1,5 +1,34 @@
 export default function Calendar() {
+  const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ]
+
+  const days = ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"]
+
   return (
-    <div>Calendar</div>
+    <div className="flex-1 space-y-4 p-4 md:p-4 pt-6">
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-bold tracking-tight">Calendario</h2>
+        <div className="flex items-center gap-2">
+          <button className="border border-gray-200 flex justify-center items-center py-2 px-3 hover:bg-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-600 mr-1">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+            Nueva Transacción
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
