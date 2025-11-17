aed673eae4cbb411ba8fc5c9f9f559c6683a5420
Juanco <juanffo874@gmail.com>
2025-11-16 21:36:47 -0600

    Muestra solo las categorias activas

--- src/features/accounts/components/TabsAccounts.jsx
+++ src/features/accounts/components/TabsAccounts.jsx
@@ -34,7 +34,7 @@ export default function TabsAccounts() {
             <div>
                 <TabGroup>
                     <TabList className="flex border border-gray-200">
-                        {categories.map(({ name }) => (
+                        {categories.filter((category => category.active == true)).map(({ name }) => (
                             <Tab
                                 key={name}
                                 className="border border-gray-200 px-3 py-1 text-gray-800 font-medium bg-gray-300 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:cursor-pointer data-selected:bg-gray-50 data-selected:data-hover:bg-gray-50"
