c5c3b139f95aff2677b6c1deb9e5a04b562d9549
Juanco <juanffo874@gmail.com>
2025-11-19 23:14:07 -0600

    feat: Crea el componente para mostrar las opciones en las transacciones

--- src/features/budgets/components/ActiveBudgets.jsx
+++ src/features/budgets/components/ActiveBudgets.jsx
@@ -1,19 +1,20 @@
+import DropDownOptions from "./DropDownOptions"
+
 export default function ActiveBudgets() {
     return (
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             <div className="p-4 border border-gray-200 space-y-2">
                 <div className="flex items-center justify-between w-full">
                     <div className="flex items-center gap-2">
-                        <div className="rounded-full bg-gray-100 p-1">
+                        <button className="rounded-full bg-gray-100 p-1">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-emerald-700">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
                             </svg>
-                        </div>
+                        </button>
                         <h2 className="font-medium">Alimentación</h2>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 hover:bg-gray-200 p-1">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                    </svg>
+                    {/*DropDownOptions*/}
+                    <DropDownOptions/>
                 </div>
                 <p className="text-sm text-gray-500">Mensual · Esencial</p>
                 <div className="space-y-1 w-full">
@@ -24,7 +25,7 @@ export default function ActiveBudgets() {
                     <progress value={350} max={500} className="h-2 w-full overflow-hidden" />
                 </div>
                 <div className="flex justify-between text-sm">
-                    <p>$120 esta semana</p>
+                    <p className="text-gray-600">$120 esta semana</p>
                     <p className="text-gray-600 text-xs">$150 disponibles</p>
                 </div>
             </div>
---
+++ src/features/budgets/components/DropDownOptions.jsx
@@ -0,0 +1,50 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+
+export default function DropDownOptions() {
+    const handleEdit = () => {
+        console.log("Editar")
+    }
+
+    const handleDelete = () => {
+        console.log("Eliminar")
+    }
+  return (
+    <div className="">
+        <Menu>
+            <MenuButton className="inline-flex items-center gap-2 font-semibold shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-700 data-open:bg-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 hover:bg-gray-200 p-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </svg>
+            </MenuButton>
+            <MenuItems
+                transition
+                anchor="bottom end"
+                className="w-32 origin-top-right shadow-md border border-gray-200 bg-white p-1 text-sm/6 text-gray-900 transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
+            >
+                <MenuItem>
+                    <button
+                        onClick={handleEdit}
+                        className="group font-medium flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                        </svg>
+                        Editar
+                    </button>
+                </MenuItem>
+                <MenuItem>
+                    <button
+                        onClick={handleDelete}
+                        className="group flex font-medium w-full items-center gap-2 rounded-lg px-3 py-1.5 data-focus:bg-white/10"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                        Eliminar
+                    </button>
+                </MenuItem>
+            </MenuItems>
+        </Menu>
+    </div>
+  )
+}
