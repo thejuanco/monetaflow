61707eb8a0a69e7b4cc438e1b948034b122a902c
Juanco <juanffo874@gmail.com>
2026-01-15 00:19:29 -0600

    feat: Agrega un combobox al formulario para mostrar opciones en el select

--- src/features/budgets/components/ModalFormAddBudget.jsx
+++ src/features/budgets/components/ModalFormAddBudget.jsx
@@ -1,6 +1,31 @@
-import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { useState } from "react"
+import { 
+    Description,
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    Combobox,
+    ComboboxInput,
+    ComboboxOption,
+    ComboboxOptions,
+    ComboboxButton
+} from '@headlessui/react'
+import clsx from "clsx"
 
 export default function ModalFormAddBudget({ isModalOpen, setIsModalOpen }) {
+    const options = [
+        {id: 1, name: "Ingreso"},
+        {id: 2, name: "Gasto"},
+        {id: 3, name: "Programado"}
+    ]
+
+    const [selectedOption, setSelectedOption] = useState(options[1])
+    const [query, setQuery] = useState("")
+
+    const filteredOptions = query === "" ? options : options.filter((op) => {
+        return op.name.toLowerCase().includes(query.toLowerCase())
+    })
+
     return (
         <Dialog
             open={isModalOpen}
@@ -34,12 +59,47 @@ export default function ModalFormAddBudget({ isModalOpen, setIsModalOpen }) {
 
                         <div>
                             <label className="font-medium">Tipo</label>
-                            <select name="" id="" className="w-full border border-gray-200 p-2">
-                                <option value="">--Selecciona una opcion--</option>
-                                <option value="">Ingreso</option>
-                                <option value="">Gasto</option>
-                                <option value="">Programado</option>
-                            </select>
+                            <Combobox 
+                                value={selectedOption}
+                                onChange={(value) => setSelectedOption(value)}
+                                onClose={() => {setQuery("")}}
+                            >
+                                <div className="relative">
+                                    <ComboboxInput
+                                        displayValue={(value) => value?.name}
+                                        onChange={(event) => setQuery(event.target.value)}
+                                        className={clsx(
+                                            "w-full bg-white/5 py-1.5 pl-3 border border-gray-300"
+                                        )}
+                                    >
+                                        
+                                    </ComboboxInput>
+                                    <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </ComboboxButton>
+                                </div>
+
+                                <ComboboxOptions
+                                    anchor="bottom"
+                                    transition
+                                    className={clsx(
+                                        'w-(--input-width) border border-gray-300 bg-white p-1 [--anchor-gap:--spacing(1)] empty:invisible',
+                                        'transition duration-100 ease-in data-leave:data-closed:opacity-0'
+                                    )}
+                                    >
+                                    {filteredOptions.map((option) => (
+                                        <ComboboxOption 
+                                            key={option.id}
+                                            value={option}
+                                            className={`group flex cursor-default items-center gap-2 px-3 py-1.5 select-none data-focus:bg-gray-100`}
+                                        >
+                                            <div className="text-gray-900">{option.name}</div>
+                                        </ComboboxOption>
+                                    ))}
+                                </ComboboxOptions>
+                            </Combobox>
                         </div>
 
                         <div>
