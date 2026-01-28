8629651cfad0e5fa24188ce3b35d9f39268d1f7b
Juanco <juanffo874@gmail.com>
2026-01-27 19:28:12 -0600

    feat: Agrega los encabezados de las pantallas de perfil y configuración

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className="flex-1 space-y-4 p-4 md:p-4 pt-6">
+      <div className="flex items-center justify-between">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight">Mi Perfil</h2>
+          <p className="text-gray-600">Administra tu información personal y preferencias de la aplicación</p>
+        </div>
+      </div>
+    </div>
   )
 }
