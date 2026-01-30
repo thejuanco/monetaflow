bafd22600a31dc80ba30659eb8b3887fc0343ce0
Juanco <juanffo874@gmail.com>
2026-01-29 21:46:47 -0600

    feat: Agrega los encabezados de las páginas

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className="flex-1 space-y-4 p-4 md:p-4 pt-6">
+        <div className="flex items-center justify-between">
+            <div>
+                <h2 className="text-3xl font-bold tracking-tight">Tu Perfil</h2>
+                <p></p>
+            </div>
+        </div>
+    </div>
   )
 }
--- src/pages/Profile/Settings.jsx
+++ src/pages/Profile/Settings.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Settings() {
   return (
-    <div>Settings</div>
+    <div className="flex-1 space-y-4 p-4 md:p-4 pt-6">
+        <div className="flex items-center justify-between">
+            <div>
+                <h2 className="text-3xl font-bold tracking-tight">Configuración</h2>
+                <p></p>
+            </div>
+        </div>
+    </div>
   )
 }
