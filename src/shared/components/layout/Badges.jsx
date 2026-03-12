59ab21ecd4db39c98b667f890400234222f17a80
Juanco <juanffo874@gmail.com>
2026-03-12 11:35:03 -0600

    feat: Modifica el badget para recibir mensajes personalizados

--- src/shared/components/layout/Badges.jsx
+++ src/shared/components/layout/Badges.jsx
@@ -1,8 +1,8 @@
-export default function Badges() {
+export default function Badges({children}) {
   return (
     <span class="inline-flex items-center rounded-md bg-blue-100 px-2 py-1 text-xs font-medium text-blue-700 inset-ring inset-ring-blue-700/10">
       <span class="h-1.5 w-1.5 bg-blue-600 rounded-full me-1"></span>
-      Preview
+      {children}
     </span>
   )
 }
